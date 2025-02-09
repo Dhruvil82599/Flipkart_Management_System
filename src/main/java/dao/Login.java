4d8a8eff9e070352bdaf8d1792eb4e7b226ac59c
dhruvil <umaretiyadhruvil.2002@gmail.com>
2025-02-09 15:09:28 +0530

    Refactor login functionality to include SQL validation and improve user feedback

--- src/main/java/dao/Login.java
+++ src/main/java/dao/Login.java
@@ -1,11 +1,13 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Login {
 
-	public static void isLogin() {
+	public static void isLogin() throws SQLException {
 
 		System.out.println("********* Welcome to Login Page *********");
 		Scanner sc = new Scanner(System.in);
@@ -20,13 +22,24 @@ public class Login {
 
 		// Write a query to check if the email and password are correct
 
-		pre
 
-		// If the email and password are correct, print "Login Successful"
-		// If the email and password are incorrect, print "Login Failed"
+		String query = "select isvalid(?,?)";
+		PreparedStatement ps = connection.prepareCall(query);
+		
+		ps.setString(1, email);
+		ps.setString(2, password);
+		
+		int i=ps.executeUpdate();
+		
+		if(i==1) {
+			System.out.println("Login Successful");
+		}
+		else {
+			System.out.println("Login Failed");
+		}
+		
+		
 
-
-
-
+		
 	}
 }
--- src/main/java/flipkart/Flipcart.java
+++ src/main/java/flipkart/Flipcart.java
@@ -1,5 +1,6 @@
 package flipkart;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 import dao.Login;
@@ -7,13 +8,13 @@ import dao.Register;
 
 public class Flipcart {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		System.out.println("Welcome to Flipcart");
 
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Press 1 to Login");
-		System.out.println("Press 2 to Legister");
+		System.out.println("Press 2 to Register");
 		System.out.println("Press 3 to Exit");
 
 		int choice = sc.nextInt();
